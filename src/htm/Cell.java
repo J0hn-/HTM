45915c0f750f0fd5379d269e642b40554ef7807a
Lilians <damien.claras@gmail.com>
2016-10-23 12:03:55 +0200

    Temporal

--- src/htm/Cell.java
+++ src/htm/Cell.java
@@ -13,6 +13,84 @@ import java.util.ArrayList;
  */
 public class Cell {
        
-    private ArrayList<Cell> neighbors;
+    private ArrayList<CellSynapse> dendrite= new ArrayList<>(); // all CellSynapse poiting on this cell
+    private boolean predictateState;
+    private boolean activeState;
+    private int dendriteThreshold;
     
+    public Cell(int DENDRITE_THRESHOLD){
+        this.predictateState = false;
+        this.activeState=false;
+        this.dendriteThreshold=DENDRITE_THRESHOLD;
+    }
+    
+    public void addCellSynapse(Cell from, Cell to, double PERMANENCE_THRESHOLD)
+    {
+        this.dendrite.add(new CellSynapse(from, to, PERMANENCE_THRESHOLD));
+    }
+    public boolean predictate(){
+        int compteur=0;
+        int predicateCompteur=0;
+        for(CellSynapse cs : dendrite)
+        {
+            if(cs.isConnected() && cs.getFrom().isActiveState())
+            {
+                compteur++;
+            }
+            if((!cs.isConnected()) && cs.getFrom().isActiveState())
+            {
+                predicateCompteur++;
+            }
+        }
+        if(predicateCompteur+compteur >= dendriteThreshold)
+        {
+           for(CellSynapse cs : dendrite)
+            {
+                cs.setActivePotentialPredictate(cs.getFrom().isActiveState());
+                cs.setDesactivePotentialPredictate(!cs.getFrom().isActiveState());
+            } 
+        }
+        if(compteur >= dendriteThreshold)
+        {
+           predictateState = true;
+           for(CellSynapse cs : dendrite)
+            {
+                cs.setActivePredictate(cs.getFrom().isActiveState());
+                cs.setDesactivePredictate(!cs.getFrom().isActiveState());
+            } 
+        }
+        return predictateState;
+    }
+
+    public ArrayList<CellSynapse> getDendrite() {
+        return dendrite;
+    }
+
+    public void setDendrite(ArrayList<CellSynapse> dendrite) {
+        this.dendrite = dendrite;
+    }
+
+    public boolean isPredictateState() {
+        return predictateState;
+    }
+
+    public void setPredictateState(boolean predictateState) {
+        this.predictateState = predictateState;
+    }
+
+    public boolean isActiveState() {
+        return activeState;
+    }
+
+    public void setActiveState(boolean activeState) {
+        this.activeState = activeState;
+    }
+
+    public int getDendriteThreshold() {
+        return dendriteThreshold;
+    }
+
+    public void setDendriteThreshold(int dendriteThreshold) {
+        this.dendriteThreshold = dendriteThreshold;
+    }
 }
